e6b8551d850083fc7de9b0995b2e3f313444405c
Lauren Davidson <laurendavidson@HUITs-MacBook-Pro-2.local>
2018-06-27 09:24:24 -0400

    new changes

--- public/javascripts/map-script.js
+++ public/javascripts/map-script.js
@@ -10,7 +10,27 @@ var data = [
     "website": "https://ofa.fas.harvard.edu/ceramics-studio",
     "tools": "pottery wheels, kilns",
     "who": "class students (including public)",
-    "tags": "exists"
+    "tags": "exists",
+    "content": '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the '+
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+            'south west of the nearest large town, Alice Springs; 450&#160;km '+
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+            'Aboriginal people of the area. It has many springs, waterholes, '+
+            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'Heritage Site.</p>'+
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>';
   },
   {
     "number": 2,
@@ -136,6 +156,7 @@ function initMap() {
   //   strokeWeight: 2
   // };
 
+
   for (var i = 0; i < data.length; i++) {
     var marker = new google.maps.Marker({
       position: {
@@ -148,6 +169,15 @@ function initMap() {
       tags: data[i].tags,
       animation: null
     });
+
+    var infowindow = new google.maps.InfoWindow({
+          content: data[i].content
+        });
+
+    marker.addListener('click', function() {
+         infowindow.open(makerMap, marker);
+       });
+
     markers.push(marker);
 
     if (data[i].number==1) {
