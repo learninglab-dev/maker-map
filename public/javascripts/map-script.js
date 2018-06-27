a51fffd0a9f70893aa93581f8ff05dcfb649fb3f
Lauren Davidson <laurendavidson@HUITs-MacBook-Pro-2.local>
2018-06-27 10:50:49 -0400

    new changes

--- public/javascripts/map-script.js
+++ public/javascripts/map-script.js
@@ -154,13 +154,12 @@ function initMap() {
     });
 
     var content = data[i].content;
-
     var infowindow = new google.maps.InfoWindow();
-    
+
     google.maps.event.addListener(marker,'click', (function(marker,content,infowindow){
     return function() {
         infowindow.setContent(content);
-        infowindow.open(map,marker);
+        infowindow.open(makerMap,marker);
     };
 })(marker,content,infowindow));
 
@@ -182,6 +181,7 @@ function filterMarkers() {
         }
         else {
             marker.setVisible(false);
+            infowindow.close(makerMap,marker);
         }
         // markers[i] = marker;
     }
