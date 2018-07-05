4fb1dce953758960d1405306538df6cbb12406db
Lauren Davidson <laurendavidson@HUITs-MacBook-Pro-2.local>
2018-07-05 16:29:01 -0400

    added geocode

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Space = require('../public/models/space.js');
+var googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GEOCODING_API_KEY,
+  Promise: Promise
+});
 
 
 router.get('/', function(req, res, next) {
@@ -8,38 +12,57 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/send', function(req, res) {
-  res.send('got your input');
   console.log('got some data');
   console.log(JSON.stringify(req.body, null, 4));
 
-  // var geocode = googleMapsClient.geocode({
-  //       address: req.body.address
-  //     }, function(err, response) {
-  //       if (!err) {
-  //         console.log(response.json.results);
-  //       }
-  //     });
+  addLocation(req.body);
 
-  var newSpace = new Space({
-    name: req.body.name,
-    address: req.body.address,
-    latitute: 0,
-    longitude: 0,
-    hours: req.body.hours,
-    website: req.body.website,
-    tags: [0, 1]
-  });
+  res.send('got your input')
 
-  console.log(JSON.stringify(newSpace, null, 4));
 
-  newSpace.save(function(err) {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log('saved to DB');
-    }
-  })
+    async function addLocation (formData) {
+      const geocode = await googleMapsClient.geocode({address: req.body.address})
+                                    .asPromise()
+                                    .then((response) => {
+                                      // console.log('the full response is:' + JSON.stringify(response, null, 4));
+                                      console.log('results are: ' + JSON.stringify(response.json.results, null, 4));
+
+                                      var latLng = {
+                                                    lat: response.json.results[0].geometry.location.lat,
+                                                    lng: response.json.results[0].geometry.location.lng
+                                                }
+                                      console.log('the coordinates are: ' + latLng.lat + ' , ' + latLng.lng);
+                                      return latLng;
+                                    })
+                                    .catch((err) => {
+                                      console.log(err);
+                                    })
+
+      console.log('here is geocode: ' + JSON.stringify(geocode, null, 4));
+
+        var newSpace = new Space({
+          name: req.body.name,
+          address: req.body.address,
+          latitute: geocode.lat,
+          longitude: geocode.lng,
+          hours: req.body.hours,
+          website: req.body.website,
+          tags: [0, 1]
+        });
+
+        console.log(JSON.stringify(newSpace, null, 4));
+
+        newSpace.save(function(err) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log('saved to DB');
+          }
+        })
+
+      }
+
 
 });
 
